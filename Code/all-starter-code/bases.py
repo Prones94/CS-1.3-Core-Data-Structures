f920dc9007be76ff8cb5decb987152b731fa8a71
Ian Rones <ronesp.85@gmail.com>
2020-04-01 17:36:43 -0700

    [edit] bases.py in starter-code, passed all tests

--- Code/all-starter-code/bases.py
+++ Code/all-starter-code/bases.py
@@ -17,33 +17,45 @@ def decode(digits, base):
     return: int -- integer representation of number (in base 10)"""
     # Handle up to base 36 [0-9a-z]
     assert 2 <= base <= 36, 'base is out of range: {}'.format(base)
+    return int(digits, base)
     # TODO: Decode digits from binary (base 2)
     # TODO: Decode digits from hexadecimal (base 16)
     # TODO: Decode digits from any base (2 up to 36)
     # digits = int(digits)
     # result = 0
     # exponent = 0
-    if base == 2:
-        '''
-        while digits > 0:
-            result += base ** exponent * (digits % 10)
-            digits //= 10
-            power += 1
-        return result
+    # if base == 2:
+    #     '''
+    #     while digits > 0:
+    #         result += base ** exponent * (digits % 10)
+    #         digits //= 10
+    #         power += 1
+    #     return result
 
-        '''
-        return int(digits, base)
-    elif base == 16:
-        '''
-        while digits > 0:
-            result += base ** exponent * (digits % 16)
-            digits //= 16
-            power += 1
-        return result
-        '''
-        return int(digits, base)
-    else:
-        return int(digits, base)
+    #     '''
+    #     return int(digits, base)
+    # elif base == 16:
+    #     '''
+    #     while digits > 0:
+    #         result += base ** exponent * (digits % 16)
+    #         digits //= 16
+    #         power += 1
+    #     return result
+    #     '''
+    #     return int(digits, base)
+    # else:
+    #     return int(digits, base)
+    #     '''
+    #     string = string[::-1]
+    #     decimal_num = 0
+    #     for exponent in range(len(string)):
+    #         single_num = string[exponent]
+    #         if single_num.isdigit():
+    #             single_num = int(single_num)
+    #         else:
+    #             single_num = ord(single_num()) - ord('A') + 10
+    #         decimal_num += single_num * (base ** exponent)
+    #     '''
 
 
 def encode(number, base):
@@ -56,20 +68,26 @@ def encode(number, base):
     # Handle unsigned numbers only for now
     assert number >= 0, 'number is negative: {}'.format(number)
     # TODO: Encode number in binary (base 2)
-    if base == 2:
-        '''
-        binary_num = 0
-        power = 0
-        while number > 0:
-            binary_num += 10 ** power *(number % base)
-            number //= 2
-            power += 1
-        return binary_num
-        '''
-    # TODO: Encode number in hexadecimal (base 16)
-    # ...
-    # TODO: Encode number in any base (2 up to 36)
-    # ...
+    # if base == 2:
+    #     return bin(number).replace("Ob", "")
+    # # TODO: Encode number in hexadecimal (base 16)
+    # elif base == 16:
+    #     return oct(number)
+    # # TODO: Encode number in any base (2 up to 36)
+    # else:
+    new_num = ""
+    while number > 0:
+        temp = int(number%base)
+        if temp < 10:
+            new_num += str(temp)
+        else:
+            new_num += chr(ord('a') + temp -10)
+        number //= base
+    
+    new_num = new_num[::-1]
+    return new_num
+    
+
 
 
 def convert(digits, base1, base2):
@@ -81,14 +99,11 @@ def convert(digits, base1, base2):
     # Handle up to base 36 [0-9a-z]
     assert 2 <= base1 <= 36, 'base1 is out of range: {}'.format(base1)
     assert 2 <= base2 <= 36, 'base2 is out of range: {}'.format(base2)
-    newNum = 0
-    exponent = 0
-    digits = int(digits)
-    while digits > 0:
-        newNum += base1 ** exponent * (digits % base2)
-        digits //= base2
-        exponent += 1
-    return newNum
+    old_string = decode(digits, base1)
+    new_string = encode(int(old_string), base2)
+    return new_string
+    
+    
 
 
 def main():
